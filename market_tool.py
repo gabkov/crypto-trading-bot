bcbef7f63e86a1b8e9cffedf46b9a0c1548138e8
Gábor Kovács <gabor950601@gmail.com>
2020-09-18 12:41:44 +0200

    add WING and BEL

--- market_tool.py
+++ market_tool.py
@@ -10,46 +10,55 @@ binance.checkRequiredCredentials()  # raises AuthenticationError
 binance_markets = binance.load_markets()
 coinbase_markets = coinbase.load_markets()
 
-coinbase_coins = []
+check_for_short_tickers = False
+short_tickers = ['HC', 'SC', 'HOT']
 
-for key in coinbase_markets.keys():
-    coinbase_coins.append(key.split('/')[0])
 
-#print(coinbase_coins)
+def get_coinbase_coins_list():
+    coins = []
+    for key in coinbase_markets.keys():
+        coins.append(key.split('/')[0])
+    return coins
 
-usdt_markets = []
-for key in binance_markets.keys():
-    if 'USDT' in key and 'UP' not in key and 'DOWN' not in key and 'BULL' not in key and 'BEAR' not in key:
-        #usdt_markets.append(key)
-        if 'HC' in key or 'SC' in key or 'HOT' in key:
-            print(key)
+def get_binance_usdt_pairs():
+    usdt_pairs = []
+    for key in binance_markets.keys():
+        if 'USDT' in key and 'UP' not in key and 'DOWN' not in key and 'BULL' not in key and 'BEAR' not in key:
+            usdt_pairs.append(key.split('/')[0])
+    return usdt_pairs
 
-        usdt_markets.append(key.split('/')[0])
-#print(usdt_markets)
 
-tickers = ['MATIC', 'OCEAN', 'STORJ', 'THETA', 'WAVES', 'DOGE', 'HBAR',
-'IOST', 'IOTA', 'KAVA', 'LEND', 'NANO',  'PAXG', 'QTUM', 'ADA', 'ANT', 'BAL',
-'BNT', 'BTS', 'BTT', 'CRV', 'DCR', 'DGB', 'DOT', 'ENJ', 'ERD',
-'FET', 'HOT', 'ICX', 'JST', 'KMD', 'LSK', 'MFT', 'NEO', 'OGN', 'ONE',
-'ONT', 'REN', 'RLC', 'RSR', 'RVN', 'SNX', 'SOL', 'SRM', 'STX', 'SXP', 'TRX',
-'VET', 'XMR', 'ZEN', 'ZIL', 'SC']
+def get_new_coins(old_list, new_list):
+    old_list = set(old_list)
+    new_list = set(new_list)
+    return list(new_list - old_list)
+
+
+coinbase_coins = get_coinbase_coins_list()
+binance_usdt_pairs = get_binance_usdt_pairs()
 
 print('############')
-goldy = set(usdt_markets) - set(coinbase_coins)
+goldy = set(binance_usdt_pairs) - set(coinbase_coins)
 goldy = list(goldy)
-
-counter = 0
-for ticker in tickers:
-    if ticker in goldy:
-        counter+=1
-
-print()
-print("Cointer: " + str(counter))
-print()
 
 goldy.sort()
 goldy.sort(key=len, reverse=True)
 filtered_goldy = list(filter(lambda x : "USD" not in x and "AUD" not in x and "GBP" not in x and "EUR" not in x and "BKRW" not in x, goldy))
 
+tickers = ['COCOS', 'MATIC', 'OCEAN', 'STORJ', 'STORM', 'STRAT', 'SUSHI', 'TFUEL', 'THETA', 
+'WAVES', 'AION', 'ANKR', 'ARDR', 'ARPA', 'BEAM', 'BZRX', 'CELR', 'COTI', 'CTSI', 'CTXC', 
+'DATA', 'DENT', 'DOCK', 'DOGE', 'DREP', 'DUSK', 'EGLD', 'HBAR', 'HIVE', 'IOST', 'IOTA', 
+'IOTX', 'IRIS', 'KAVA', 'LEND', 'LUNA', 'MITH', 'NANO', 'NPXS', 'NULS', 'PAXG', 'PERL', 
+'QTUM', 'RUNE', 'SAND', 'STMX', 'STPT', 'TOMO', 'TROY', 'VITE', 'VTHO', 'WING', 'WNXM', 'YFII', 
+'ADA', 'ANT', 'BAL', 'BCC', 'BEL', 'BLZ', 'BNB', 'BNT', 'BSV', 'BTS', 'BTT', 'CHR', 'CHZ', 'COS', 
+'CRV', 'DCR', 'DGB', 'DIA', 'DOT', 'ENJ', 'ERD', 'FET', 'FIO', 'FTM', 'FTT', 'FUN', 'GTO', 
+'GXS', 'HOT', 'ICX', 'JST', 'KEY', 'KMD', 'KSM', 'LSK', 'LTO', 'MBL', 'MCO', 'MDT', 
+'MFT', 'MTL', 'NEO', 'NKN', 'OGN', 'ONE', 'ONG', 'ONT', 'PNT', 'REN', 'RLC', 'RSR', 
+'RVN', 'SNX', 'SOL', 'SRM', 'STX', 'SXP', 'TCT', 'TRB', 'TRX', 'VEN', 'VET', 'WAN', 'WIN', 
+'WRX', 'WTC', 'XMR', 'XZC', 'ZEN', 'ZIL', 'HC', 'SC']
+
 print(filtered_goldy)
+print(len(tickers))
 print(len(filtered_goldy))
+new_ticker_list = get_new_coins(tickers, filtered_goldy)
+print(f"New coins: {new_ticker_list}")
--- twitter_poller.py
+++ twitter_poller.py
@@ -13,13 +13,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-#DCR (BTC nagyobb volume)?!
 tickers = ['COCOS', 'MATIC', 'OCEAN', 'STORJ', 'STORM', 'STRAT', 'SUSHI', 'TFUEL', 'THETA', 
 'WAVES', 'AION', 'ANKR', 'ARDR', 'ARPA', 'BEAM', 'BZRX', 'CELR', 'COTI', 'CTSI', 'CTXC', 
 'DATA', 'DENT', 'DOCK', 'DOGE', 'DREP', 'DUSK', 'EGLD', 'HBAR', 'HIVE', 'IOST', 'IOTA', 
 'IOTX', 'IRIS', 'KAVA', 'LEND', 'LUNA', 'MITH', 'NANO', 'NPXS', 'NULS', 'PAXG', 'PERL', 
-'QTUM', 'RUNE', 'SAND', 'STMX', 'STPT', 'TOMO', 'TROY', 'VITE', 'VTHO', 'WNXM', 'YFII', 
-'ADA', 'ANT', 'BAL', 'BCC', 'BLZ', 'BNB', 'BNT', 'BSV', 'BTS', 'BTT', 'CHR', 'CHZ', 'COS', 
+'QTUM', 'RUNE', 'SAND', 'STMX', 'STPT', 'TOMO', 'TROY', 'VITE', 'VTHO', 'WING', 'WNXM', 'YFII', 
+'ADA', 'ANT', 'BAL', 'BCC', 'BEL', 'BLZ', 'BNB', 'BNT', 'BSV', 'BTS', 'BTT', 'CHR', 'CHZ', 'COS', 
 'CRV', 'DCR', 'DGB', 'DIA', 'DOT', 'ENJ', 'ERD', 'FET', 'FIO', 'FTM', 'FTT', 'FUN', 'GTO', 
 'GXS', 'HOT', 'ICX', 'JST', 'KEY', 'KMD', 'KSM', 'LSK', 'LTO', 'MBL', 'MCO', 'MDT', 
 'MFT', 'MTL', 'NEO', 'NKN', 'OGN', 'ONE', 'ONG', 'ONT', 'PNT', 'REN', 'RLC', 'RSR', 
